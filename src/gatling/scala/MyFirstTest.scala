5c5027c7f08a644840be7ecfd785a2c4e37e91a6
Giovanni A <giovanni.arsenius@unisuper.com.au>
2019-03-18 14:35:11 +1100

    Updated MyFirstTest with a load profile and added rampup, holdFor etc..

--- src/gatling/scala/MyFirstTest.scala
+++ src/gatling/scala/MyFirstTest.scala
@@ -4,15 +4,35 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 class MyFirstTest extends BaseSimulation {
+  //load profile
+  val t_iterations = Integer.getInteger("iterations", 100).toInt
+  val t_concurrency = Integer.getInteger("concurrency", 10).toInt
+  val t_rampUp = Integer.getInteger("ramp-up", 1).toInt
+  val t_holdFor = Integer.getInteger("hold-for", 60).toInt
+  val t_throughput = Integer.getInteger("throughput", 100).toInt
 
   //2. Scenario definition
-  val scn = scenario("JSONPlaceholder Typicode")
-    .exec(http("Get All Posts")
+  // 'forever' means each thread will execute scenario until
+  // duration limit is reached
+  val loopScenario  = scenario("JSONPlaceholder Typicode").forever(){
+    exec(http("Get All Posts")
       .get("posts"))
-    .pause(5)
+      //.pause(5)
+  }
+
+  // if you want to set an iteration limit (instead of using duration limit),
+  // you can use the following scenario
+  /*val iterationScenario = scenario("Iteration Scenario").repeat(t_iterations) {
+    exec(http("Get All Posts").get("/"))
+  }*/
+
+  val execution = loopScenario
+    .inject(rampUsers(t_concurrency) over t_rampUp)
+    .protocols(httpConf)
+
 
   //3. Load Profile
-  setUp(
-    scn.inject(atOnceUsers(1))
-  ).protocols(httpConf)
+  setUp(execution).
+    throttle(jumpToRps(t_throughput), holdFor(t_holdFor)).
+    maxDuration(t_rampUp + t_holdFor)
 }
