0f05715278dab2d6e2d05a6f6c027bc6665a72bb
Giovanni A <giovanni.arsenius@unisuper.com.au>
2019-03-27 09:33:27 +1100

    Added scenario to extract data and save to a variable

--- src/gatling/scala/simulations/CheckResponseBodyAndExtract.scala
+++ src/gatling/scala/simulations/CheckResponseBodyAndExtract.scala
@@ -10,9 +10,20 @@ class CheckResponseBodyAndExtract extends BaseSimulation{
   val scn = scenario("JSON Placeholder Typicode Posts")
 
     //Verify response body using JSONPath
+    //Verify Title for Post Id 1 is json-server
     .exec(http("Get PostID 1")
       .get("posts/1")
       .check(jsonPath("$.title").is("json-server")))
+
+//Get all posts and extract Post id 2 and save in variable `postId'
+      .exec(http("Get All Posts")
+      .get("posts")
+      .check(jsonPath("$[1].id").saveAs("postId")))
+
+//Get specific post. In this case Post Id 2
+    .exec(http("Get Specific Post")
+      .get("posts/${postId}")
+      .check(jsonPath("$.title").is("json-server-2")))
 
   //define load
   setUp(
